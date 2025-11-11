ed3a13ffb54ed0dc480995ad8654713c0184ab73
Aymeric-Thibert <130760614+Aymeric-Thibert@users.noreply.github.com>
2025-11-11 10:00:53 -0500

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,76 @@
 import { Component } from '@angular/core';
 import {transition, trigger, useAnimation} from "@angular/animations";
+import { pulse, shakeX } from 'ng-animate';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+  trigger("death", [
+    transition(
+      ":increment",
+      useAnimation(shakeX, { params: { timing: 0.5 } })
+    ),
+  ]),
+   trigger('attack', [
+      transition(':increment', [
+        useAnimation( pulse, {params: {timing: 0.3, scale: 4.5}}),
+      ]),
+    ]),
+],
+
 })
 export class AppComponent {
   slimeIsPresent = false;
-
+  ng_death = 0;
+  ng_scale = 0;
+  css_hit= false;
   constructor() {
   }
 
   spawn() {
     this.slimeIsPresent = true;
     // TODO Animation angular avec forwards
+    this.showSlime()
   }
 
   death(){
     this.slimeIsPresent = false;
     // TODO Animation angular avec forwards
+    this.hideSlime()
 
     // TODO 2e animation angular en même temps
+    this.ng_death++
+    
+    
   }
 
   attack(){
     // TODO Jouer une animation et augmenter l'intensité du mouvement avec scale
+    this.ng_scale ++
+    setTimeout(() => this.ng_scale++,200)
+
     // TODO Jouer une autre animation avant
   }
 
   hit(){
     // TODO Utilisé Animista pour faire une animation différente avec css (wobble)
+    this.css_hit =true;
+    setTimeout(() => this.css_hit = false, 0.3 * 1000);
   }
+  showSlime(){
+    var element = document.getElementById("slimeyId");
+    element?.classList.remove("fadeOut");
+    element?.classList.add("fadeIn");
+}
+  hideSlime(){
+    var element =document.getElementById("slimeyId");
+    element?.classList.add("fadeOut");
+    element?.classList.remove("fadeIn");
+
+
+  }
+
+  
 }
