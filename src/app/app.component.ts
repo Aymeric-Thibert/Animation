fefce6e8407e9437653d3d1005e408aa490ee044
Aymeric-Thibert <130760614+Aymeric-Thibert@users.noreply.github.com>
2025-11-11 10:18:03 -0500

    fdgfd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {transition, trigger, useAnimation} from "@angular/animations";
-import { pulse, shakeX } from 'ng-animate';
+import { bounce, flip, pulse, shakeX } from 'ng-animate';
+import { lastValueFrom, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -18,6 +19,21 @@ import { pulse, shakeX } from 'ng-animate';
         useAnimation( pulse, {params: {timing: 0.3, scale: 4.5}}),
       ]),
     ]),
+    trigger('bounce', [
+      transition(':increment', [
+        useAnimation( bounce, {params: {timing: 1}}),
+      ]),
+    ]),
+    trigger('shake', [
+      transition(':increment', [
+        useAnimation( shakeX, {params: {timing: 0.75}}),
+      ]),
+    ]),
+    trigger('flip', [
+      transition(':increment', [
+        useAnimation( flip, {params: {timing: 0.75}}),
+      ]),
+    ]),
 ],
 
 })
@@ -25,6 +41,10 @@ export class AppComponent {
   slimeIsPresent = false;
   ng_death = 0;
   ng_scale = 0;
+  ng_bounce = 0;
+  ng_shake =0;
+  ng_flip =0;
+
   css_hit= false;
   constructor() {
   }
@@ -58,6 +78,18 @@ export class AppComponent {
     // TODO Utilisé Animista pour faire une animation différente avec css (wobble)
     this.css_hit =true;
     setTimeout(() => this.css_hit = false, 0.3 * 1000);
+  }
+
+  async BSF(){
+
+     this.ng_bounce++;
+    await lastValueFrom(timer(1 * 1000));
+    this.ng_shake++;
+    await lastValueFrom(timer(0.75 * 1000));
+    this.ng_flip++;
+
+
+
   }
   showSlime(){
     var element = document.getElementById("slimeyId");
